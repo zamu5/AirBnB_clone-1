3bd5f956038407a3604f509ed1677e971e803e1a
Sergio Zamudio <szamu5@hotmail.com>
2019-12-17 14:38:06 -0500

    [FEATURE]: Implement SQLALchemy New file: db_storage.py Modified: state.py, city.py, base_model.py and __init__.py

--- models/city.py
+++ models/city.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = Column(String(60),  ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
---
+++ models/engine/db_storage.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+from os import environ
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class DBStorage():
+    """
+    """
+    __engine = None
+    __session = None
+    cla = [State, City]
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
+        if 'HBNB_ENV' in environ and environ['HBNB_ENV'] == 'test':
+            Base.metadata.drop_all(bind=self.__engine)
+
+    def all(self, cls=None):
+        dic = {}
+        if cls is None:
+            for c in self.cla:
+                row = self.__session.query(c).all()
+                for k in row:
+                    key = ".".join([cls, k.id])
+                    dic.update({key: k})
+            return dic
+        else:
+            row = self.__session.query(eval(cls)).all()
+            for k in row:
+                key = ".".join([cls, k.id])
+                dic.update({key: k})
+            return dic
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(Session)()

7101e890442e9cb392cd47d8cc4fa640756b4bc8
Karen Herrera <837@holbertonschool.com>
2019-12-18 15:33:36 +0000

    number 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from models.place import Place, place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary="place_amenity")
--- models/place.py
+++ models/place.py
@@ -4,7 +4,11 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship, backref
 from os import environ
+from sqlalchemy import Table, Text
 
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """This is the class for Place
@@ -35,6 +39,7 @@ class Place(BaseModel, Base):
     amenity_ids = []
     if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
         reviews = relationship('Review', backref="places")
+        amenities = relationship('Amenity', secondary="place_amenity", back_populates='place_amenities', viewonly=False)
     else:
         lis = []
 
@@ -45,3 +50,16 @@ class Place(BaseModel, Base):
                 if "Review" in k and v.state_id == self.id:
                     lis.append(v)
             return lis
+
+        @property
+        def amenities(self):
+            stat = self.id
+            for k, v in models.storage.all().items():
+                if "Amenity" in k and v.amenity_id == self.id:
+                    lis.append(v)
+            return lis
+
+        @amenities.setter
+        def amenities(self, value):
+            if value.__class__.__name__ == "Amenity":
+                self.amenity_id.append(str(value.id))

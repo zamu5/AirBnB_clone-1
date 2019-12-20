7cc5b046d674f231b8143d0afd712e6dc933bcdc
Karen Herrera <837@holbertonschool.com>
2019-12-20 19:17:30 +0000

    tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -56,6 +56,8 @@ class TestCity(unittest.TestCase):
         self.assertEqual(type(self.city.name), str)
         self.assertEqual(type(self.city.state_id), str)
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                     'Not supported in Data Base mode')
     def test_save_City(self):
         """test if the save works"""
         self.city.save()

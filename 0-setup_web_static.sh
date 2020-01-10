a992dd6883c5f431a66345099efcddb9a19d0801
Sergio Zamudio <szamu5@hotmail.com>
2020-01-10 00:04:21 -0500

    FIX: shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 str='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}'

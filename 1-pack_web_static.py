f94f73bd183e7d44f6975c8e4a4f0de7f371021a
Sergio zamudio <szamu5@hotmail.com>
2020-01-10 15:46:43 +0000

    FIX: check pep8 style Modified: 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,12 +3,14 @@ import os
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     try:
         if not os.path.exists("versions"):
             local('mkdir versions')
         tim = datetime.now()
-        name_file = "versions/web_static_{}.tgz".format(tim.strftime("%Y%m%d%H%M%S"))
+        fort = "%Y%m%d%H%M%S"
+        name_file = "versions/web_static_{}.tgz".format(tim.strftime(fort))
         local('tar -cvzf {} web_static'.format(name_file))
         return name_file
     except:

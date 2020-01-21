00cc48b436239036ff6ae62263de119e56bf212f
Sergio Zamudio <szamu5@hotmail.com>
2020-01-21 15:57:18 -0500

    STYLE: check pep8 style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,12 +3,14 @@
 flask module
 """
 from flask import Flask
-
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def main():
+    """
+    Method returns a message
+    """
     return ("Hello HBNB!")
 
 if __name__ == "__main__":
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,16 +3,24 @@
 flask module
 """
 from flask import Flask
-
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def main():
+    """
+    method that returns a message when look for localhost:5000/
+    """
     return ("Hello HBNB!")
+
 
 @app.route('/hbnb')
 def hbnb():
+    """
+     method that returns a message when look for localhost:5000/hbnb
+    """
     return ("HBNB")
+
+
 if __name__ == "__main__":
     app.run()

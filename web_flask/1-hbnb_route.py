6789dd5ce7bd59ebbbe248324007da4ab17f8a09
Sergio Zamudio <szamu5@hotmail.com>
2020-01-21 15:48:18 -0500

    FEAT: Add task 0 and 1, also upload the README file.
    Upload the new task that create development server
    
    New file:  0-hello_route.py, 1-hbnb_route.py, README.md

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+flask module
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def main():
+    return ("Hello HBNB!")
+
+@app.route('/hbnb')
+def hbnb():
+    return ("HBNB")
+if __name__ == "__main__":
+    app.run()
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+python3 -m web_flask.1-hbnb_route

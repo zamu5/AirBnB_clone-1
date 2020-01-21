4ead797ef598095e3026c7f1ca2e53501c7cf76c
Sergio Zamudio <szamu5@hotmail.com>
2020-01-21 16:28:09 -0500

    FEAT: Add method taht handle variable text
    This new method handle Variable Rules
    
    New file:  2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+flask module
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    """
+    method that returns a message when look for localhost:5000/
+    """
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+     method that returns a message when look for localhost:5000/hbnb
+    """
+    return ("HBNB")
+
+
+@app.route('/C/<text>', strict_slashes=False)
+def variable(text):
+    """
+    This method handle a variable text
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run()
